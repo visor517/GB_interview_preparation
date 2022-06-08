9b25fb759c313775a8e496b0cdd9b474ba770466
visor517 <visor517@yandex.ru>
2022-06-08 13:13:45 +0300

    task3 done

--- lesson3/task3.py
+++ lesson3/task3.py
@@ -4,3 +4,15 @@
 словаре для него должно сохраняться значение None. Если есть  значения, которым не хватило ключей, их необходимо 
 отбросить.
 '''
+
+def create_dict(keys: list, values: list):
+    if len(keys) > len(values):
+        for _ in range(len(keys) - len(values)):
+            values.append(None)
+            
+    return {key: values[i] for i, key in enumerate(keys)}
+
+
+print(create_dict(['A', 'B', 'C', 'D'], [1, 2, 3, 4, 5]))
+print(create_dict(['A', 'B', 'C', 'D', 'E'], [1, 2, 3, 4, 5]))
+print(create_dict(['A', 'B', 'C', 'D', 'E'], [1, 2, 3, 4]))
